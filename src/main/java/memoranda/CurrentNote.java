e83f89bd4f01f5c70c3196723bca77ba8ddc1af3
task1 <Charles@>
2021-03-16 21:02:31 +0500

    first change

--- src/main/java/memoranda/CurrentNote.java
+++ src/main/java/memoranda/CurrentNote.java
@@ -15,8 +15,7 @@ public class CurrentNote {
     public static void set(Note note, boolean toSaveCurrentNote) {
         noteChanged(note, toSaveCurrentNote);
         currentNote = note;
-//second change //
-//third change //
+
     }
 
     public static void reset() {
